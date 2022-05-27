b114c0b72fdc01ed3fa05ca042324979921666d5
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-05-27 11:06:04 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+function Quebec(){
+  return <h1>Quebec</h1>
+}
+function Montreal(){
+  return <h1>Montreal</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper App">
+      <BrowserRouter>
+      <div>
+        <Link to='/quebec'><button>Quebec</button></Link>
+        <Link to='/montreal'><button>Montreal</button></Link>
+      </div>
+      <Switch>
+        <Route path='/quebec'>
+          <Quebec/>
+        </Route>
+        <Route path='/montreal'>
+          <Montreal/>
+        </Route>
+
+      </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
