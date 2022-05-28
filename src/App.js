95f7968ec759aaf5af5d0cab5d2a014a9ac58d8b
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-05-28 22:56:58 +0300

    creating dropdown

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import './menu/menu.css';
+import Base from './Base';
+import Dropdown from './Dropdown'
 import Quebec from './content/Quebec'
 import Montreal from './content/Montreal';
 import Home from './content/Home'
 import Form from './content/Form'
 function App() {
-  return (
-    <div className="wrapper ">
-      <BrowserRouter>
-      <h1 id="top" className='p-5 rounded '>Туристический сайт по Квебеку</h1>
-        
-        <div className="stick">
-        <nav>
-        <Link to='/quebec'><button class="btn">Квебек Сити</button></Link>
-        <Link to='/montreal'><button class="btn">Монреаль</button></Link>
-        <Link to='/order'><button class="btn">Заказать</button></Link>
-        <Link to='/'><button class="btn">Home</button></Link>
-        </nav>
-        </div>
-      
-      <Switch>
-        <Route path='/quebec'>
-          <Quebec/>
-        </Route>
-        <Route path='/montreal'>
-          <Montreal/>
-        </Route>
-        <Route path='/order'>
-           <Form/>
-        </Route>
-        <Route path='/'>
-          <Home/>
-
-        </Route>
-
-      </Switch>
-      </BrowserRouter>
-
-    </div>
-  );
+  let w=window.innerWidth;
+  return <div>
+    <Base/>
+  </div>
 }
 
 export default App;
---
+++ src/Dropdown.js
@@ -0,0 +1,50 @@
+import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
+import Quebec from './content/Quebec'
+import Montreal from './content/Montreal';
+import Home from './content/Home'
+import Form from './content/Form'
+function Dropdown(){
+
+
+    return (
+        <div className="wrapper " >
+            
+          <BrowserRouter>
+          <h1 id="top" className='p-5 rounded '>Туристический сайт по Квебеку</h1>
+          <div className="stick">
+              
+              <nav id="nav">
+                
+              <Link to='/quebec'><button class="btn">Квебек Сити</button></Link><hr/>
+              <Link to='/montreal'><button class="btn">Монреаль</button></Link><hr/>
+              <Link to='/order'><button class="btn">Заказать</button></Link><hr/>
+              <Link to='/'><button class="btn">Home</button></Link><hr/>
+              </nav>
+              </div>
+    
+          
+          <Switch>
+            <Route path='/quebec'>
+              <Quebec/>
+            </Route>
+            <Route path='/montreal'>
+              <Montreal/>
+            </Route>
+            <Route path='/order'>
+               <Form/>
+            </Route>
+            <Route path='/'>
+              <Home/>
+    
+            </Route>
+    
+          </Switch>
+          </BrowserRouter>
+    
+        </div>
+
+
+      );
+
+}
+export default Dropdown;
