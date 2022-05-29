ed6a09c76ea60cac7461f6713f1b1359093db91a
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-05-29 23:12:01 +0300

    ch

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,24 +1,43 @@
 import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
+import React, { useState } from "react";
 import Quebec from './content/Quebec'
 import Montreal from './content/Montreal';
 import Home from './content/Home'
-import Form from './content/Form'
-function Dropdown(){
+import Form from './content/Form';
 
+
+function Dropdown(){
+  let i=1;
+  function myF(){
+    i++;
+    if(i%2==0){
+      document.getElementById("navd").style.display="none";
+    }
+    else{document.getElementById("navd").style.display="block";
+    document.getElementById("navd").style.position="fixed";
+  }
+  }
+  function Diss(){
+    i--
+    document.getElementById("navd").style.display="none"
+  }
 
     return (
-        <div className="wrapper " >
-            
+
+        <div class="wrapper">
+           
           <BrowserRouter>
-          <h1 id="top" className='p-5 rounded '>Quebecc</h1>
+ 
+          <h1 id="top" className='p-5 rounded '>Quebec</h1>
+          <button onClick={myF} className="btn">+</button>
           <div className="stick">
               
-              <nav id="nav">
+              <nav  id="navd" onClick={Diss}>
                 
-              <Link to='/quebec'><button class="btn">Квебек Сити</button></Link><hr/>
-              <Link to='/montreal'><button class="btn">Монреаль</button></Link><hr/>
-              <Link to='/order'><button class="btn">Заказать</button></Link><hr/>
-              <Link to='/'><button class="btn">Home</button></Link><hr/>
+              <Link to='/quebec'><button class="btn q"><h3>Квебек Сити</h3></button></Link><hr/>
+              <Link to='/montreal'><button class="btn q">Монреаль</button></Link><hr/>
+              <Link to='/order'><button class="btn q">Заказать</button></Link><hr/>
+              <Link to='/'><button class="btn q">Home</button></Link><hr/>
               </nav>
               </div>
     
